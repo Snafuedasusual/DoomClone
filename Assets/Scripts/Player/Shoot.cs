7e9d1504cb5b3c7c417911d9be26a1002d3a9844
Snafuedasusual <omarsnafu04@gmail.com>
2023-12-29 22:13:28 +0700

    V1.051
    -Added basic shotgun spread, no response yet.

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+
+    public Camera plrCam;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,26 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _Shoot();
+    }
+
+    private void _Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int _sht = 0; _sht < 16; _sht++)
+            {
+                Ray _shotFired = plrCam.ViewportPointToRay(new Vector3(Random.Range(0.2f, 0.8f), Random.Range(0.2f, 0.8f), 0f));
+                RaycastHit _shotHit;
+                if (Physics.Raycast(_shotFired, out _shotHit))
+                {
+                    Debug.Log("I hit " + _shotHit.transform.name);
+                }
+                else
+                {
+                    Debug.Log("Miss!");
+                }
+            }
+        }
     }
 }
